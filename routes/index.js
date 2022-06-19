0134e39b120187acab7995c5e145cb9e872b2f6b
shanu sharma <shanu04012003@gmail.com>
2022-06-19 11:34:52 +0530

    added user controller and router

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const homeController = require('../controllers/home_controller');
 console.log('router loded');
 
 router.get('/', homeController.home);
+router.use('/users',require('./users'));
 module.exports = router;
